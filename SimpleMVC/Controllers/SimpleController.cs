6a0b5aefe74d9f28eff65a9960785f94600f941e
Krzysztof Szwajka <krzysztof.szwajka@student.wsg.pl>
2024-01-19 23:49:10 +0100

    Add project files.

---
+++ SimpleMVC/Controllers/SimpleController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMVC.Models;
+
+namespace SimpleMVC.Controllers
+{
+    public class SimpleController : Controller
+    {
+        // GET: SimpleController
+        private List<Person>? persons;
+        public ActionResult Index()
+        {
+            // SimpleExemple exemples = new SimpleExemple("trangle", 3.4f, 4.6f);
+            persons = new List<Person>()
+            {
+                new Person() {Id = 1, FirstName = "Jan", LastName = "Kowalski", Age = 25, IdIncome = 1, IdSpending = 1},
+                new Person() {Id = 2, FirstName = "Ewa", LastName = "Farna", Age = 54, IdIncome = 2, IdSpending = 2},
+                new Person() {Id = 3, FirstName = "Stanisław", LastName = "Kamiński", Age = 45, IdIncome = 3, IdSpending = 3},
+                new Person() {Id = 5, FirstName = "Janina", LastName = "Kowalskia", Age = 17, IdIncome = 4, IdSpending = 4},
+                new Person() {Id = 6, FirstName = "Jurek", LastName = "Nowak", Age = 12, IdIncome = 5, IdSpending = 5}
+
+            };
+
+            
+            //  exemples.Add(new SimpleExemple("trangle",3,5)); 
+            return View(persons);
+        }
+
+        // GET: SimpleController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SimpleController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SimpleController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SimpleController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SimpleController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SimpleController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SimpleController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SimpleMVC/Models/SimpleExemple.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace SimpleMVC.Models
+{
+
+    #region Person
+    public class Person
+    {
+        private readonly int id;
+        public int Id { get; set; }
+
+        private string? firstname;
+        public string FirstName { get; set; } = string.Empty;
+
+        private string? lastname;
+        public string LastName { get; set; } = string.Empty;
+
+        private int? age;
+
+        public int? Age { get; set; }
+
+        private int idincome;
+
+        public int? IdIncome { get; set; }
+
+        private int idspending;
+
+        public int? IdSpending { get; set; }
+    }
+    #endregion
+
+    #region Income
+    public class Income
+    {
+        private int id;
+        public int Id { get; set; }
+
+        private float salary;
+        public float Salary { get; set; }
+
+        private float sidejob;
+        public float SideJob { get; set; }
+
+        private float rental;
+        public float Rental { get; set;}
+        
+        private DateTime? date;
+        public DateTime? Date { get; set; } 
+
+    }
+    #endregion
+
+    #region Spending
+    public class Spending
+    {
+        private int id;
+        public int Id { get; set; }
+
+        private float bills;
+        public float Bills { get; set; }
+        
+        private float food;
+        public float Food { get; set; }
+        
+        private float drugs;
+        public float Drugs { get; set; }
+
+        private float loans;
+        public float Loans { get; set; }
+
+        private float holidays;
+        public float Holidays { get; set; }
+
+        private float unexpected;
+        public float Unexpected { get; set; }
+
+        private DateTime date;
+        public DateTime Date { get; set; }
+        
+    }
+
+    #endregion
+
+
+    #region SimpleExemple
+    public class SimpleExemple
+    {
+        private string shape;
+
+        public string? Shape { get; set; }
+
+        private float girth;
+
+        public float? Girth { get; set; }
+        
+        private float area;
+
+        public float? Area { get; set; }
+
+       public SimpleExemple(string shape, float girth, float area)
+        {
+            this.shape = shape;
+            this.girth = girth;
+            this.area = area;
+        }
+
+        public SimpleExemple()
+        {
+            this.shape = String.Empty;
+            this.girth = 0;
+            this.area = 0;
+        }
+    }
+    #endregion 
+}
