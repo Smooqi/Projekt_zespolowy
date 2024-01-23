05788bd3063a7e04e9fb41492ff0a19964fe936c
Wercis619 <weragajkowska@gmail.com>
2024-01-23 22:53:31 +0100

    dodanie formularzy i stylizacja formularzy

--- SimpleMVC/wwwroot/js/site.js
+++ SimpleMVC/wwwroot/js/site.js
@@ -56,7 +56,6 @@ function wybierzOpcje(opcja) {
     }
 
     if (przyciskAktywowany) {
-        przyciskAktywowany.innerText = opcja;
         przyciskAktywowany.disabled = true;
         odblokujPrzyciski();
     }
@@ -104,6 +103,212 @@ function zapiszZwykly() {
     odblokujPrzyciski();
 }
 
+function zobaczFormularzStaly() {
+    // Ukryj przyciski dodatkowe
+    var dodatkowePrzyciski1 = document.getElementById('dodatkowePrzyciski1');
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'none';
+    przyciskPrzychod.style.display = 'none';
+    przyciskOszczednosc.style.display = 'none';
+    dodatkowePrzyciski1.style.display = 'none';
+
+    // Pokaż formularz zwykłego wydatku
+    var formularz = document.getElementById('formularzStalyWydatek');
+    formularz.style.display = 'block';
+}
+
+function zapiszStaly() {
+    var nazwa = document.getElementById('nazwaStalegoWydatku').value;
+    var ile = document.getElementById('ileStalegoWydatku').value;
+    var kiedy = document.getElementById('kiedyStalegoWydatku').value;
+    var data = document.getElementById('dataStalegoWydatku').value;
+
+    // Tutaj możesz dodać kod obsługujący zapis wydatku
+    // ...
+
+    // Po zapisie ukryj formularz
+    var formularz = document.getElementById('formularzStalyWydatek');
+    formularz.style.display = 'none';
+
+    // Po zapisie przywróć widoczność przycisków dodatkowych
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'block';
+    przyciskPrzychod.style.display = 'block';
+    przyciskOszczednosc.style.display = 'block';
+
+    odblokujPrzyciski();
+}
+
+function zobaczFormularzZwyklyPrzychod() {
+    // Ukryj przyciski dodatkowe
+    var dodatkowePrzyciski2 = document.getElementById('dodatkowePrzyciski2');
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'none';
+    przyciskPrzychod.style.display = 'none';
+    przyciskOszczednosc.style.display = 'none';
+    dodatkowePrzyciski2.style.display = 'none';
+
+    // Pokaż formularz zwykłego przychodu
+    var formularz = document.getElementById('formularzZwyklyPrzychod');
+    formularz.style.display = 'block';
+}
+
+function zapiszZwyklyPrzychod() {
+    var nazwa = document.getElementById('nazwaZwyklegoPrzychodu').value;
+    var ile = document.getElementById('ileZwyklegoPrzychodu').value;
+    var data = document.getElementById('dataZwyklegoPrzychodu').value;
+
+    // Tutaj możesz dodać kod obsługujący zapis przychodu
+    // ...
+
+    // Po zapisie ukryj formularz
+    var formularz = document.getElementById('formularzZwyklyPrzychod');
+    formularz.style.display = 'none';
+
+    // Po zapisie przywróć widoczność przycisków dodatkowych
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'block';
+    przyciskPrzychod.style.display = 'block';
+    przyciskOszczednosc.style.display = 'block';
+
+    odblokujPrzyciski();
+}
+
+function zobaczFormularzStalyPrzychod() {
+    // Ukryj przyciski dodatkowe
+    var dodatkowePrzyciski2 = document.getElementById('dodatkowePrzyciski2');
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'none';
+    przyciskPrzychod.style.display = 'none';
+    przyciskOszczednosc.style.display = 'none';
+    dodatkowePrzyciski2.style.display = 'none';
+
+    // Pokaż formularz stałego przychodu
+    var formularz = document.getElementById('formularzStalyPrzychod');
+    formularz.style.display = 'block';
+}
+
+function zapiszStalyPrzychod() {
+    var nazwa = document.getElementById('nazwaStalegoPrzychodu').value;
+    var ile = document.getElementById('ileStalegoPrzychodu').value;
+    var kiedy = document.getElementById('kiedyStalegoPrzychodu').value;
+    var data = document.getElementById('dataStalegoPrzychodu').value;
+
+    // Tutaj możesz dodać kod obsługujący zapis stałego przychodu
+    // ...
+
+    // Po zapisie ukryj formularz
+    var formularz = document.getElementById('formularzStalyPrzychod');
+    formularz.style.display = 'none';
+
+    // Po zapisie przywróć widoczność przycisków dodatkowych
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'block';
+    przyciskPrzychod.style.display = 'block';
+    przyciskOszczednosc.style.display = 'block';
+
+    odblokujPrzyciski();
+}
+function zobaczFormularzZwyklaOszczednosc() {
+    // Ukryj przyciski dodatkowe
+    var dodatkowePrzyciski3 = document.getElementById('dodatkowePrzyciski3');
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'none';
+    przyciskPrzychod.style.display = 'none';
+    przyciskOszczednosc.style.display = 'none';
+    dodatkowePrzyciski3.style.display = 'none';
+
+    // Pokaż formularz zwykłej oszczędności
+    var formularz = document.getElementById('formularzZwyklaOszczednosc');
+    formularz.style.display = 'block';
+}
+
+function zapiszZwyklaOszczednosc() {
+    var nazwa = document.getElementById('nazwaZwyklejOszczednosci').value;
+    var ile = document.getElementById('ileZwyklejOszczednosci').value;
+    var data = document.getElementById('dataZwyklejOszczednosci').value;
+
+    // Tutaj możesz dodać kod obsługujący zapis oszczędności
+    // ...
+
+    // Po zapisie ukryj formularz
+    var formularz = document.getElementById('formularzZwyklaOszczednosc');
+    formularz.style.display = 'none';
+
+    // Po zapisie przywróć widoczność przycisków dodatkowych
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'block';
+    przyciskPrzychod.style.display = 'block';
+    przyciskOszczednosc.style.display = 'block';
+
+    odblokujPrzyciski();
+}
+
+function zobaczFormularzStalaOszczednosc() {
+    // Ukryj przyciski dodatkowe
+    var dodatkowePrzyciski3 = document.getElementById('dodatkowePrzyciski3');
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'none';
+    przyciskPrzychod.style.display = 'none';
+    przyciskOszczednosc.style.display = 'none';
+    dodatkowePrzyciski3.style.display = 'none';
+
+    // Pokaż formularz stałej oszczędności
+    var formularz = document.getElementById('formularzStalaOszczednosc');
+    formularz.style.display = 'block';
+}
+
+function zapiszStalaOszczednosc() {
+    var nazwa = document.getElementById('nazwaStalejOszczednosci').value;
+    var ile = document.getElementById('ileStalejOszczednosci').value;
+    var kiedy = document.getElementById('kiedyStalejOszczednosci').value;
+    var data = document.getElementById('dataStalejOszczednosci').value;
+
+    // Tutaj możesz dodać kod obsługujący zapis stałej oszczędności
+    // ...
+
+    // Po zapisie ukryj formularz
+    var formularz = document.getElementById('formularzStalaOszczednosc');
+    formularz.style.display = 'none';
+
+    // Po zapisie przywróć widoczność przycisków dodatkowych
+    var przyciskWydatek = document.getElementById('przyciskWydatek');
+    var przyciskPrzychod = document.getElementById('przyciskPrzychod');
+    var przyciskOszczednosc = document.getElementById('przyciskOszczednosc');
+
+    przyciskWydatek.style.display = 'block';
+    przyciskPrzychod.style.display = 'block';
+    przyciskOszczednosc.style.display = 'block';
+
+    odblokujPrzyciski();
+}
 
 
 
